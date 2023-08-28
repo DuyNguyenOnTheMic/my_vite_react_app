1f70e11b3602fb136ebae28c8d8113c574b64680
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-28 14:28:23 +0700

    Update Typescript quay of create root element React

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
+ReactDOM.createRoot(rootElement).render(<App />)
