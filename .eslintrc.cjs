72ecc8679a03ae0991bf66a4994ddf64170f6b7c
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-08-28 13:38:05 +0700

    Add major files for project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,62 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2024: true, node: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'prettier'
   ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react/display-name': 'off',
+    '@next/next/no-img-element': 'off',
+    'react/no-unescaped-entities': 'off',
+    'import/no-anonymous-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // add new line above comment
+    'lines-around-comment': [
+      'error',
+      {
+        beforeLineComment: true,
+        beforeBlockComment: true,
+        allowBlockStart: true,
+        allowClassStart: true,
+        allowObjectStart: true,
+        allowArrayStart: true
+      }
+    ],
+    // add new line above return
+    'newline-before-return': 'error',
+    // add new line below import
+    'import/newline-after-import': [
+      'error',
+      {
+        count: 1
+      }
+    ],
+    '@typescript-eslint/ban-types': [
+      'error',
+      {
+        extendDefaults: true,
+        types: {
+          '{}': false
+        }
+      }
+    ]
   },
-};
+  plugins: ['import'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['./tsconfig.json']
+      }
+    }
+  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+}
