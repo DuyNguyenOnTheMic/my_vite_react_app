6c9f215aeaf71cc4b90088e6def270917549bd61
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-09-04 10:52:28 +0700

    Update banner code

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from 'react'
+import { ArrowRightCircle } from 'react-bootstrap-icons'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import HeaderImg from '../assets/img/header-img.svg'
 
 const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer']
+  const [text, setText] = useState('')
+  const [delta, setDelta] = useState(300 - Math.random() * 100)
+  const period = 2000
+
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick()
+    }, delta)
+
+    return () => {
+      clearInterval(ticker)
+    }
+  }, [text])
+
+  const tick = () => {
+    const i = loopNum % toRotate.length
+    const fullText = toRotate[i]
+    const updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
+
+    setText(updatedText)
+
+    if (isDeleting) {
+      setDelta((preDelta: number) => preDelta / 2)
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true)
+      setDelta(period)
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false)
+      setLoopNum(loopNum => loopNum + 1)
+      setDelta(500)
+    }
+  }
+
   return (
     <section className='banner' id='home'>
       <Container>
@@ -10,17 +51,19 @@ const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className='tagline'>Welcome to my Portfolio</span>
             <h1>
-              Hi I'm Duy Nguyen<span className='wrap'>Web Developer</span>
+              Hi I'm Duy Nguyen <span className='wrap'>{text}</span>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                 industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
                 scrambled it to make a type specimen book
               </p>
             </h1>
-            <button onClick={() => console.log('connect')}>Let's Connect</button>
+            <button onClick={() => console.log('connect')}>
+              Let's Connect <ArrowRightCircle />
+            </button>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={''} alt='Header Image' />
+            <img src={HeaderImg} alt='Header Image' />
           </Col>
         </Row>
       </Container>
