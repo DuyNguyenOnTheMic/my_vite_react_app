60e97be5441f1d2819b41c9a3df9583a630e8f9e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-09-07 09:33:04 +0700

    Add mailchimp form component

---
+++ src/components/MailchimpForm.tsx
@@ -0,0 +1,7 @@
+const MailchimpForm = () => {
+  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`
+
+  return <></>
+}
+
+export default MailchimpForm
