d7e8c3d1f9ce0bcd40202bd01d1ecf7301904f1f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-09-08 11:03:22 +0700

    Update url for subscribe mailchimp

--- src/components/MailchimpForm.tsx
+++ src/components/MailchimpForm.tsx
@@ -1,21 +1,18 @@
-import { Fragment } from 'react'
 import MailchimpSubscribe from 'react-mailchimp-subscribe'
 import Newsletter from './Newsletter'
 
 const MailchimpForm = () => {
-  const postUrl = `${import.meta.env.REACT_APP_MAILCHIMP_URL}?u=${import.meta.env.REACT_APP_MAILCHIMP_U}&id=${
-    import.meta.env.REACT_APP_MAILCHIMP_ID
-  }`
+  const postUrl = `https://gmail.us21.list-manage.com/subscribe/post?u=${
+    import.meta.env.VITE_REACT_APP_MAILCHIMP_U
+  }&id=${import.meta.env.VITE_REACT_APP_MAILCHIMP_ID}`
 
   return (
-    <Fragment>
-      <MailchimpSubscribe
-        url={postUrl}
-        render={({ subscribe, status, message }) => (
-          <Newsletter status={status} message={message} onValidated={(formData: any) => subscribe(formData)} />
-        )}
-      ></MailchimpSubscribe>
-    </Fragment>
+    <MailchimpSubscribe
+      url={postUrl}
+      render={({ subscribe, status, message }) => (
+        <Newsletter status={status} message={message} onValidated={(formData: any) => subscribe(formData)} />
+      )}
+    ></MailchimpSubscribe>
   )
 }
 
