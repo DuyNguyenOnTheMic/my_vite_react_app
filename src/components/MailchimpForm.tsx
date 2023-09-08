af4ca747e67cf481e21793c527430b1d4472d305
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-09-08 10:33:50 +0700

    Update process.env in Vite

--- src/components/MailchimpForm.tsx
+++ src/components/MailchimpForm.tsx
@@ -3,7 +3,9 @@ import MailchimpSubscribe from 'react-mailchimp-subscribe'
 import Newsletter from './Newsletter'
 
 const MailchimpForm = () => {
-  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`
+  const postUrl = `${import.meta.env.REACT_APP_MAILCHIMP_URL}?u=${import.meta.env.REACT_APP_MAILCHIMP_U}&id=${
+    import.meta.env.REACT_APP_MAILCHIMP_ID
+  }`
 
   return (
     <Fragment>
